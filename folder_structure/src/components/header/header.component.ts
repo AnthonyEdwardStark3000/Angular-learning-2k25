edf6ef204f8ab482ba501934c16c9f211cffc9ef
Suresh Babu J <sureshbabuj@student.tce.edu>
2025-03-04 22:05:32 +0530

    Angular 19 starter project

---
+++ folder_structure/src/components/header/header.component.ts
@@ -0,0 +1,8 @@
+import {Component} from '@angular/core';
+@Component({
+    selector:'app-header',
+    styles:[],
+    standalone:true,
+    templateUrl:'./header.component.html'
+})
+export class HeaderComponent{}
