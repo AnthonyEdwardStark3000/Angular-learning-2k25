ca7c0c7753f58521f8497461e0c06e0e43a6b0fe
Suresh Babu J <sureshbabuj@student.tce.edu>
2025-03-07 01:21:28 +0530

    String interpolation and property binding

---
+++ 02-Dynamic-data/src/app/user/user.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { DUMMY_USERS } from '../Dummy_users';
+
+const randomIndex =  Math.floor(Math.random() * DUMMY_USERS.length);
+@Component({
+  selector: 'app-user',
+  imports: [],
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.scss',
+})
+export class UserComponent {
+  selectedUser = DUMMY_USERS[randomIndex];
+  get imagePath(){
+    return 'assets/users/'+this.selectedUser.avatar;
+  }
+}
